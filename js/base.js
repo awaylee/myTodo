125c0e3413d9f9ca9e8e884e11847b3ee4da186a
waylee <969603910@qq.com>
2017-08-08 18:15:38 +0800

    删除myTodo mask

--- js/base.js
+++ js/base.js
@@ -7,14 +7,23 @@
 
   var $form_add_task = $('.add-task'),
       new_task = {},
-      task_list = {}
+      task_list = {},
+      $delete_task
       ;
 
 
-
-
+  /**
+  * 初始化数据
+  * */
   init();
-  $form_add_task.on('submit',function (e) {
+
+
+/**
+* 监听添加新的task submit事件
+* */
+  $form_add_task.on('submit',on_add_task_form_submit);
+
+  function on_add_task_form_submit(e) {
     //var  new_task = {};
     /*禁用默认行为*/
     e.preventDefault();
@@ -24,10 +33,22 @@
     if(!new_task.content) return;
     /*存入新的task*/
     if (add_task(new_task)){
-      render_task_list();
+      // render_task_list();
       $input.val('');
     }
-  });
+  }
+
+  /**
+  * 监听删除按钮的点击事件
+  * */
+  function listen_delete_task() {
+    $delete_task.on('click',function () {
+      /*找到删除元素所在的index*/
+      var index = $(this).parents('.task-item').data('index');
+      var _tmp = confirm('确认删除？');
+      _tmp ? delete_task(index) : null;
+    });
+  }
 
   function init() {
     task_list = store.get('task_list') || [];
@@ -36,37 +57,72 @@
     }
   }
 
+  /**
+   * 添加task
+   * */
   function add_task(new_task) {
     /*将新的task推入task_list*/
     task_list.push(new_task);
     /*更新localStorage的值*/
-    store.set('task_list',task_list);
-
-    console.log('task_list',task_list);
+    refresh_task_list();
     return true;
   }
 
+  /**
+   * 删除一条task
+   * */
+  function delete_task(index){
+    /*如果没有index，或index不存在就直接返回*/
+    if(index == undefined || !task_list[index]) return;
+
+    delete task_list[index];
+    /*更新localStorage的值*/
+    refresh_task_list();
+
+
+  }
+  /**
+   * 刷新localStorage的数据并渲染模板
+   * */
+  function refresh_task_list() {
+    store.set('task_list',task_list);
+    render_task_list();
+  }
+
+  /**
+   * 渲染全部task模板
+   * */
   function render_task_list() {
     var $task_list = $('.task-list');
     $task_list.html('');
 
     for (var i = 0; i <task_list.length; i++) {
-      var $task = render_task_tpl(task_list[i]);
+      var $task = render_task_item(task_list[i],i);
       $task_list.append($task);
     }
+
+    $delete_task = $('.delete');
+    listen_delete_task();
+
   }
 
-  function render_task_tpl(data) {
-    var list_item_tpl = '<div class="task-item">\
-                        <span><input type="checkbox"></span>\
-                        <span class="task-content">'+data.content+'</span>\
-                        <span class="fr">\
-                          <span class="action"> 删除</span>\
-                          <span class="action">详细</span>\
-                        </span>\
-                      </div>';
+  /**
+   * 渲染单挑task模板
+   * */
+  function render_task_item(data,index) {
+    if(!data || !index) return;
+    var list_item_tpl = '<div class="task-item" data-index="'+index+'">\
+                          <span><input type="checkbox"></span>\
+                          <span class="task-content">'+data.content+'</span>\
+                          <span class="fr">\
+                            <span class="action delete"> 删除</span>\
+                            <span class="action detail">详细</span>\
+                          </span>\
+                        </div>';
     return $(list_item_tpl);
   }
+
+
 
 
 })();
